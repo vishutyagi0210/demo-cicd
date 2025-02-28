4d1f271fd809b05920672479bbb542a959630e25
Vishal <vt3630@gmail.com>
2025-02-28 15:10:31 +0530

    new one

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { prismaClient } from "db/client";
+
+export default async function Home() {
+  const users = await prismaClient.user.findMany();
+  return (
+    <div>
+      {JSON.stringify(users)}
+    </div>
+  );
+}
+
+export const revalidate = 60 // revalidate every 60 seconds
+// or
+// export const dynamic = 'force-dynamic'
